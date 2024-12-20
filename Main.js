3f29abaad63a128d2d73d734c737ddb5de3bbe3b
Shaik Babita Begam <134837781+shaikbabita369@users.noreply.github.com>
2024-12-20 23:24:23 +0530

    Create Element and nested elements

---
+++ Main.js
@@ -0,0 +1,5 @@
+var y= React.createElement("div",null,React.createElement("h2",null,"Hello World Program using react"),
+        React.createElement("p",null,"This is nested react element"));
+        var htmlRoot = document.getElementById("root");
+        var rootReact = ReactDOM.createRoot(htmlRoot);
+        rootReact.render(y);
